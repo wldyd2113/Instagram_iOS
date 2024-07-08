2292c5b26636db21d0a3949f2f023e728f008f98
jiyong <wldyd2113@gmail.com>
2024-07-08 17:34:09 +0900

    feat: SceneDelegate 수정

--- Instagram_iOS/Instagram_iOS/SceneDelegate.swift
+++ Instagram_iOS/Instagram_iOS/SceneDelegate.swift
@@ -20,8 +20,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        let viewController = ViewController()
-        let navigationController = UINavigationController(rootViewController: viewController)
+        let loginViewController = LoginViewController()
+        let navigationController = UINavigationController(rootViewController: loginViewController)
         
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
