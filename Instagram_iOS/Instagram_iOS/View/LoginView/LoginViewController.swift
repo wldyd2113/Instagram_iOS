74ab11e373261fbfc75a1d6b32b148675d31c7d0
jiyong <wldyd2113@gmail.com>
2024-07-08 17:36:51 +0900

    UIKit에서 preview사용

--- Instagram_iOS/Instagram_iOS/View/LoginView/LoginViewController.swift
+++ Instagram_iOS/Instagram_iOS/View/LoginView/LoginViewController.swift
@@ -5,25 +5,43 @@
 //  Created by 차지용 on 7/8/24.
 //
 
-import UIKit
 
+import UIKit
+import SwiftUI
 class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
+
+
+
+
+struct PreView: PreviewProvider {
+    static var previews: some View {
+        LoginViewController().toPreview()
+    }
+}
+
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+            let viewController: UIViewController
+
+            func makeUIViewController(context: Context) -> UIViewController {
+                return viewController
+            }
+
+            func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+            }
+        }
+
+        func toPreview() -> some View {
+            Preview(viewController: self)
+        }
+}
+#endif
