99f0822823bdfa073e812a6426121d5aa7582595
jiyong <wldyd2113@gmail.com>
2024-07-08 17:18:54 +0900

    Initiaalize Instagram Project

---
+++ Instagram_iOS/Instagram_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram_iOS
+//
+//  Created by 차지용 on 7/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
