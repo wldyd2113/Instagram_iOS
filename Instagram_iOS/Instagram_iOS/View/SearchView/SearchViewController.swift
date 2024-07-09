d29ab43675a4709bd8a9835ad9946d18768b5cdd
jiyong <wldyd2113@gmail.com>
2024-07-09 17:07:10 +0900

    feat: 각 그룹마다 View생성

---
+++ Instagram_iOS/Instagram_iOS/View/SearchView/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  Instagram_iOS
+//
+//  Created by 차지용 on 7/9/24.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .blue
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Instagram_iOS/Instagram_iOS/View/UploadFeedView/UploadFeedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  UploadFeedViewController.swift
+//  Instagram_iOS
+//
+//  Created by 차지용 on 7/9/24.
+//
+
+import UIKit
+
+class UploadFeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .green
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
